6a69d48cbde4300fd099b7000289f9f9a4c9d418
Finn Coffey <finn.coffey@gmail.com>
2022-10-30 18:02:47 +1100

    feat: set up a window

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use macroquad::prelude::*;
+
+fn window_conf() -> Conf {
+    Conf {
+        window_title: "Dungeon Oxide".into(),
+        high_dpi: true,
+        ..Default::default()
+    }
+}
+
+#[macroquad::main(window_conf)]
+async fn main() {
+    loop {
+        clear_background(BLACK);
+        draw_text("Hello, world!", 10.0, 20.0, 20.0, WHITE);
+
+        next_frame().await
+    }
 }
